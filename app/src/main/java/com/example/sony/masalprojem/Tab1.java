b39f917f5b4c56556c44c93d3b72e40ce372388c
ahmetcuneydioglu <canahmettt@gmail.com>
2015-03-29 23:12:20 +0300

    Initial commit

---
+++ app/src/main/java/com/example/sony/masalprojem/Tab1.java
@@ -0,0 +1,28 @@
+package com.example.sony.masalprojem;
+
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
+
+/**
+ * Created by hp1 on 21-01-2015.
+ */
+public class Tab1 extends Fragment{
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.tab_1, container, false);
+        return v;
+    }
+
+
+
+
+}
