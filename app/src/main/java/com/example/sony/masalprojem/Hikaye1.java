143a6870828db0e07333426fc666038d25027165
ahmetcuneydioglu <canahmettt@gmail.com>
2015-04-01 00:44:44 +0300

    deneme kaldırıldı

--- app/src/main/java/com/example/sony/masalprojem/Hikaye1.java
+++ app/src/main/java/com/example/sony/masalprojem/Hikaye1.java
@@ -4,7 +4,6 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.PersistableBundle;
-import android.support.v7.app.ActionBarActivity;
 import android.view.View;
 import android.widget.Button;
 
@@ -14,7 +13,6 @@ import android.widget.Button;
 public class Hikaye1 extends Activity {
 
     Button btnAnasayfa;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,7 +28,5 @@ public class Hikaye1 extends Activity {
                 startActivity(intent);
             }
         });
-
-
     }
 }
